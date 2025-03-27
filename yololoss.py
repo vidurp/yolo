ea244a03b1ef7cd0d1c457d6075e9865043eac35
vidurp <168379404+vidurp@users.noreply.github.com>
2025-03-27 00:53:29 -0500

    Improved iou function
    allows for a set of bounding boxes to be evaluated at once

--- yololoss.py
+++ yololoss.py
@@ -3,54 +3,34 @@ import tensorflow as tf
 # Add a tf.function decorator, allows for graph execution
 @tf.function
 def CalcIOU( BBoxTrue, BBoxPred ):
-    """
-    Function that Calculates Intersection Over Union between two 
-    Bounding Boxes. Bounding Boxes arguments are supplied as tuples
-    ( x, y, w, h )
-    
+    '''
+    Calculates Intersection over union IOU
+    Supplied bounding boxes have general shape [ N , 4 ] where each element
+    is of format x,y,w,h
+
     Args:
-        BBoxTrue: Truth Value Bounding Box
-        BBoxPred: Predicted Bounding Box
-        
-    Returns: IOU value, if there is no overlap, 
-             returns 0.01 as a base floor
-    """
-    xtrue = BBoxTrue[0]
-    ytrue = BBoxTrue[1]
-    wtrue = BBoxTrue[2]
-    htrue = BBoxTrue[3]
-    xpred = BBoxPred[0]
-    ypred = BBoxPred[1]
-    wpred = BBoxPred[2]
-    hpred = BBoxPred[3]
+        BBoxTrue - Truth Bounding Box(es)
+        BBoxPred - Predicted Bounding Box(es)
 
-    AUnion = 0
-    AInter = 0
+    Returns:
+        A tensor of shape (N, ) containing IOU values
+    '''
+    AreaBBoxTrue = BBoxTrue[...,2] * BBoxTrue[...,3]
+    AreaBBoxPred = BBoxPred[...,2] * BBoxPred[...,3]
 
-    if ( wpred == 0 or hpred == 0 or wtrue == 0 or htrue == 0 ):
-        iou = tf.constant(0.01, dtype=tf.float32)
-    # check for no overlap
-    elif ( ( (( xpred + wpred ) <  xtrue ) or ( (xpred + wpred) > (xtrue + wtrue) ) ) and
-         ( (( ypred + hpred ) <  ytrue ) or ( (ypred + hpred) > (ytrue + htrue) ) ) ):
-        iou = tf.constant(0.01, dtype=tf.float32)
-    else:
-        # Intersection
-        Atrue = tf.math.multiply( wtrue , htrue )
-        Apred = tf.math.multiply( wpred , hpred )
-        AInter = 0
+    # top left coordinates of intersection
+    xtop = tf.maximum( BBoxTrue[...,0], BBoxPred[...,0] )
+    ytop = tf.maximum( BBoxTrue[...,1], BBoxPred[...,1] )
 
-        xinter = tf.math.maximum( xtrue, xpred )
-        yinter = tf.math.maximum( ytrue, ypred )
-        xinter2 = tf.math.minimum( xtrue + wtrue, xpred + wpred )
-        yinter2 = tf.math.minimum( ytrue + htrue, ypred + hpred )
-        AInter = tf.math.abs( tf.math.multiply( ( xinter - xinter2 ) , ( yinter - yinter2 ) ) )
+    # bottom right coordinates of intersection
+    xbottom =  tf.minimum( BBoxTrue[...,0] + BBoxTrue[...,2], BBoxPred[...,0] + BBoxPred[...,2] )
+    ybottom =  tf.minimum( BBoxTrue[...,1] + BBoxTrue[...,3], BBoxPred[...,1] + BBoxPred[...,3] )
 
-        AUnion = tf.math.add(Atrue , Apred )
-        AUnion = tf.math.subtract( AUnion,  AInter )
-        iou = tf.math.divide( AInter , AUnion )
+    AreaInterSection = tf.maximum( xbottom - xtop, 0.0 ) * tf.maximum( ybottom - ytop, 0.0 )
 
-    return (tf.floor(iou * 100) / 100)
-    
+    AUnion = AreaBBoxPred + AreaBBoxTrue - AreaInterSection
+    iou = AreaInterSection / AUnion
+    return iou
     
 @tf.function
 def YOLOLoss( Ytrue , Ypred ):
