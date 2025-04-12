05b89358c2d3a227b378b4201f592a960a8c63f3
vidurp <168379404+vidurp@users.noreply.github.com>
2025-04-12 18:43:52 -0500

    remove need to reshape tensor
    Output layer in TF model can use Reshape() to make outputs in the correct shape

--- yololoss.py
+++ yololoss.py
@@ -37,29 +37,28 @@ def YoloLoss( Ytrue , Ypred ):
     """
     Loss Function for YOLO Algorithm
     Args:
-        Ytrue: Truth Labels
-        YPred: Predicted Labels
+        Ytrue: Truth Labels ( Batch x 7 x 7 x 17 )
+        YPred: Predicted Labels ( Batch x 7 x 7 x 17 ) 
         
+        Two predictors pers cell. Each pred is a BBOX + Confidence + 7 classes
+        17 -> ( 4 + 1 ) x 2 + 7
+         
     Returns: Loss Value
     """
-    # Passing -1 to first arg will automatically deduce batch size
-    # and reshape PredT, TrueT correctly
-    PredT = tf.reshape( Ypred , ( -1,7,7,17 ) )
-    TrueT = tf.reshape( Ytrue , ( -1,7,7,17 ) )
-
-    ClassTrue = tf.reshape( TrueT[...,10:] , shape=(-1,7) )
-    ClassPred = tf.reshape( PredT[...,10:], shape=(-1,7) )
+    ClassTrue = tf.reshape( Ytrue[...,10:], shape=(-1,7) )
+    ClassPred = tf.reshape( Ypred[...,10:], shape=(-1,7) )
 
     # change 7x7xN into 49 x N 1D samples
-    BBox1True = tf.reshape( TrueT[...,0:4], shape=(-1,4) )
-    Conf1True = tf.reshape( TrueT[...,4], shape=(-1,) ) 
-    BBox2True = tf.reshape( TrueT[...,5:9], shape=(-1,4 ) )
-    Conf2True = tf.reshape( TrueT[...,9], shape=(-1,) )
+    BBox1True = tf.reshape( Ytrue[...,0:4], shape=(-1,4) )
+    Conf1True = tf.reshape( Ytrue[...,4], shape=(-1,) )
+    BBox2True = tf.reshape( Ytrue[...,5:9], shape=(-1,4 ) )
+    Conf2True = tf.reshape( Ytrue[...,9], shape=(-1,) )
 
-    BBox1Pred = tf.reshape( PredT[...,0:4], shape=(-1,4) )
-    Conf1Pred = tf.reshape( PredT[...,4], shape=(-1,) ) 
-    BBox2Pred = tf.reshape( PredT[...,5:9], shape=(-1,4) )
-    Conf2Pred = tf.reshape( PredT[...,9], shape=(-1,) )
+    BBox1Pred = tf.reshape( Ypred[...,0:4], shape=(-1,4) )
+    Conf1Pred = tf.reshape( Ypred[...,4], shape=(-1,) )
+    BBox2Pred = tf.reshape( Ypred[...,5:9], shape=(-1,4) )
+    Conf2Pred = tf.reshape( Ypred[...,9], shape=(-1,) )
+
 
     # BBox variables now have shape ( Batch x 49 , 4 ) for iou calculations
     iou1 = Conf1True * CalcIOU( BBox1True, BBox1Pred )
@@ -72,20 +71,21 @@ def YoloLoss( Ytrue , Ypred ):
     # confidence loss from both predictors
     ConfidenceLossPredictor1 = ValidPredictor1 * tf.square( Conf1True - Conf1Pred )
     ConfidenceLossPredictor2 = ValidPredictor2 * tf.square( Conf2True - Conf2Pred )
-    Loss = tf.reduce_sum( ConfidenceLossPredictor1, axis = 0 )
-    Loss += tf.reduce_sum( ConfidenceLossPredictor2, axis = 0 )
+    Loss = tf.reduce_mean( ConfidenceLossPredictor1, axis = 0 )
+    Loss += tf.reduce_mean( ConfidenceLossPredictor2, axis = 0 )
 
     # Classification Loss
-    ClassificationLoss = Conf1True * tf.cast(tf.square( tf.argmax(ClassTrue,axis=1) -  tf.argmax(ClassPred,axis=1) ), dtype=tf.float32 ) 
+    ClassificationLoss = Conf1True * tf.cast(tf.square( tf.argmax(ClassTrue,axis=1) -  tf.argmax(ClassPred,axis=1) ), dtype=tf.float32 )
+
     Loss += tf.reduce_sum( ClassificationLoss, axis = 0 )
 
     # Bounding Box Loss
-    BBoxLossPredictor1 = ValidPredictor1 * tf.reduce_sum( tf.square( BBox1True[:,:2] - BBox1Pred[:,:2] ) + 
-                                                          tf.square( tf.sqrt(BBox1True[:,2:]) - tf.sqrt(BBox1Pred[:,2:]) ), axis = 1 )
-    
-    BBoxLossPredictor2 = ValidPredictor2 * tf.reduce_sum( tf.square( BBox2True[:,:2] - BBox2Pred[:,:2] ) + 
+    BBoxLossPredictor1 = ValidPredictor1 * tf.reduce_sum( tf.square( BBox1True[:,:2] - BBox1Pred[:,:2] ) +
+                                                         tf.square( tf.sqrt(BBox1True[:,2:]) - tf.sqrt(BBox1Pred[:,2:]) ), axis = 1 )
+
+    BBoxLossPredictor2 = ValidPredictor2 * tf.reduce_sum( tf.square( BBox2True[:,:2] - BBox2Pred[:,:2] ) +
                                                           tf.square( tf.sqrt(BBox2True[:,2:]) - tf.sqrt(BBox2Pred[:,2:]) ), axis = 1 )
-    
+
 
     Loss += tf.reduce_sum( BBoxLossPredictor1, axis = 0 )
     Loss += tf.reduce_sum( BBoxLossPredictor2, axis = 0 )
